d7d84789b174fd8c5216ab13b617d9cac7ff8ffd
xingzhi <xingzhi2@illinois.edu>
2020-11-09 11:13:10 -0600

    tests

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,37 +1,45 @@
 var request = require('request')
-
+host = 7777
+username = 'abel'
 describe('calc', () => {
     it('should multiply 2 and 2', () => {
         expect(2 * 2).toBe(4)
     })
 })
 
-describe('get messages', () => {
+describe('post user info', () => {
     it('should return 200 Ok', (done) => {
-        request.get('http://localhost:3000/messages', (err, res) => {
-            expect(res.statusCode).toEqual(200)
-            done()
-        })
-    })
-    it('should return a list, thats not empty', (done) => {
-        request.get('http://localhost:3000/messages', (err, res) => {
-            expect(JSON.parse(res.body).length).toBeGreaterThan(0)
+        request.post({
+            url: `http://localhost:${host}/user/${username}`,
+            json: true,
+            body: {
+                "username": "abel",
+                "email": "abel@something.com",
+                "password": "password",
+                "rank": "3D"
+            }
+        }, (err, res) => {
+            expect(res.statusCode).toEqual(201)
+            // console.log(res.statusCode)
             done()
         })
     })
 })
 
-describe('get messages from user', () => {
+describe('get user info', () => {
     it('should return 200 Ok', (done) => {
-        request.get('http://localhost:3000/messages/tim', (err, res) => {
+        request.get(`http://localhost:${host}/user/${username}`, (err, res) => {
             expect(res.statusCode).toEqual(200)
+            // console.log(res.statusCode)
             done()
         })
     })
-    it('name should be tim', (done) => {
-        request.get('http://localhost:3000/messages/tim', (err, res) => {
-            expect(JSON.parse(res.body)[0].name).toEqual('tim')
+    it('should return a list, thats not empty', (done) => {
+        request.get(`http://localhost:${host}/user/${username}`, (err, res) => {
+            // console.log(res)
+            expect(JSON.parse(res.body).username).toEqual(username)
             done()
         })
     })
 })
+
---
+++ spec/support/util.spec.js
@@ -0,0 +1,29 @@
+const sgf = require("@sabaki/sgf");
+const GameTree = require("@sabaki/crdt-gametree/src/GameTree");
+const {calcScoreHeuristic} = require("../../utils/helpers");
+const {getBoard} = require("../../utils/gametree");
+
+describe('calc score', () => {
+    let getId = (id => () => id++)(0)
+    let rootNodes = sgf.parseFile('sgfs/complete.sgf')
+    let gameTrees = rootNodes.map(rootNode => {
+        return new GameTree({getId, root: rootNode})
+    })
+
+    it('this game has 276 moves plus 1 end move', () => {
+        expect(gameTrees[0].getHeight()).toBe(277)
+    })
+
+    let scoreBoard = getBoard(gameTrees[0], 276).clone()
+    it('should give us following score', async () => {
+        let r = await calcScoreHeuristic(scoreBoard)
+        expect(JSON.stringify(r)).toBe(JSON.stringify({
+                area: [ 149, 211 ],
+                territory: [ 56, 89 ],
+                captures: [ 14, 43 ],
+                areaScore: -69.5,
+                territoryScore: -69.5
+            })
+        )
+    })
+})
