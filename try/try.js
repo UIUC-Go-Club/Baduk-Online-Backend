da2081c4ffb01c84b574660f2c673a334d821e53
xingzhi <xingzhi2@illinois.edu>
2020-11-15 01:22:40 -0600

    regret work

---
+++ try/try.js
@@ -0,0 +1,7 @@
+var hatred = "666"
+var myDict = {}
+console.log("gg")
+module.exports = function (){
+    console.log(hatred)
+    console.log(myDict)
+}
---
+++ try/try2.js
@@ -0,0 +1,1 @@
+const what = require('./try')()
